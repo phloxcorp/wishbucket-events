d0328be49381e2d1a98766f3ebd4e410e59c0ce0
jadeart <mjp3041@gmail.com>
2023-02-03 01:40:37 +0900

    tagcategorypg added

---
+++ tagcategory/action.js
@@ -0,0 +1,15 @@
+
+const highlightLinka =(e) =>{
+    const linkas = document.querySelectorAll(".sidebarsection");
+    const sections = document.querySelectorAll(".sections");
+    linkas.forEach(linka => {
+        linka.classList.remove("active");
+      });
+
+    sections.forEach(section => {
+        section.classList.add("section_hide");
+      });
+    e.target.parentNode.classList.add("active");
+    const thesection = document.querySelector(e.target.hash);
+    thesection.classList.remove("section_hide");
+}
