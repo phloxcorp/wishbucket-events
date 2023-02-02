b3b93dcad83c233d3237e3aa33184c8e81aabcca
jadeart <mjp3041@gmail.com>
2023-02-03 02:26:57 +0900

    changedtobtn

--- tagcategory/action.js
+++ tagcategory/action.js
@@ -1,5 +1,6 @@
 
 const highlightLinka =(e) =>{
+
     const linkas = document.querySelectorAll(".sidebarsection");
     const sections = document.querySelectorAll(".sections");
     linkas.forEach(linka => {
@@ -10,6 +11,6 @@ const highlightLinka =(e) =>{
         section.classList.add("section_hide");
       });
     e.target.parentNode.classList.add("active");
-    const thesection = document.querySelector(e.target.hash);
+    const thesection = document.querySelector(e.target.id);
     thesection.classList.remove("section_hide");
 }
